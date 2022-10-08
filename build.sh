72f1e4fbea71e3184c52d3afda8f1d19f94f865a
rancortooth <rancortooth@gmail.com>
2022-10-08 09:23:52 -0400

    update

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ rm -rf docs/
 rm -rf .angular/cache/
 ng run StarshipFluke:prerender
 
-cd docs
-python3 -m http.server
-
-#git status
-#git add .; git commit -m "update"; git push
+if [ "$1" == "run" ]; then
+    cd docs
+    python3 -m http.server
+fi
