cb637ac375ba3658ad03efac61ae8ff720ef8cf2
rancortooth <rancortooth@gmail.com>
2023-06-04 16:46:17 -0400

    update

--- src/app/app-post/app-post.component.ts
+++ src/app/app-post/app-post.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { PostService } from '../services/post.service';
 import { delay, switchMap, tap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post',
@@ -10,8 +11,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AppPostComponent {
   #postService = inject(PostService);
-  postUrl: string | undefined;
+  // postUrl: string | undefined;
   postImgUrl: string | undefined;
+  md: string | undefined;
+
+  constructor(private http: HttpClient) { }
 
   // tap and delay are a hack until this is resolved: https://github.com/angular/angular/issues/47813
   post$ = inject(ActivatedRoute).params.pipe(
@@ -19,11 +23,10 @@ export class AppPostComponent {
     switchMap(({ title }) => this.#postService.getPostDetails(title)),
     delay(0),
     tap(({ link, image, title }) => {
-      this.postUrl = `/_assets/posts/${link}.md`;
+      this.http.get('/_assets/posts/' + link + '.md', { responseType: 'text' }).subscribe(data => {
+        this.md = data;
+      });
       this.postImgUrl = `/assets/images/${image}`;
-      // this.#pageHeadService.setTitle(title);
-      // this.#pageHeadService.setOpenGraphTags(title, image, `/blog/post/${link}`);
-      // this.#pageHeadService.setTwitterCardData(title, image);
     }
     ),
   );
